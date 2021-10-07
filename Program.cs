4fcca0da12c869cda3d508120ed893eb17f1bd81
Fauxbear95 <danielgfaubert@gmail.com>
2021-10-07 12:44:44 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Psim.Particles;
+using Psim.Geometry2D;
+using Psim.ModelComponents;
+
+using System;
+using System.Collections.Generic;
+
+namespace Psim
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Cell c = new Cell(10, 10);
+
+			//c.AddIncPhonon(new Phonon(1));
+			//c.AddPhonon(new Phonon(1));
+
+            //Console.WriteLine(c);
+		}
+	}
+}
